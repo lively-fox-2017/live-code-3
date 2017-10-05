4a05ccfe6f0ca777c012bbe86fe5c919ea55674c
= <pangkeyd@gmail.com>
2017-10-05 11:30:24 +0700

    cekpoint2

--- models/subjects.js
+++ models/subjects.js
@@ -1,12 +1,23 @@
 const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database('./db/data.db')
+const Teacher = require('./teachers')
 
 class Subject {
 
 	constructor(data){
+		this.id = data['id']
 		this.teacher_id = data['teacher_id']
 		this.subject_name = data['subject_name']
 		this.subject_code = data['subject_code']
+	}
+
+	getTeacher(cb){
+
+		Teacher.findById(this.teacher_id)
+		.then(function(teacher){
+			cb(teacher[0])
+		})
+
 	}
 
 	static findAll(){
@@ -20,12 +31,105 @@ class Subject {
 					return new Subject(row)
 				})
 
+				let count = 0
+				subjects.forEach(function(subject){
+					subject.getTeacher(function(teacher){
+						subject.first_name = teacher.first_name
+						subject.last_name = teacher.last_name
+
+						count++
+						if(!err){
+							resolve(subjects)
+						}else{
+							reject(err)
+						}
+
+					})
+				})
+
+			})
+
+		})
+
+		return promise
+
+	}
+
+	static findById(id){
+
+		let promise = new Promise(function(resolve, reject){
+
+			let query = `SELECT * FROM Subjects WHERE id = ${id}`
+
+			db.all(query, function(err, rows){
+				let subjects = rows.map(function(row){
+					return new Subject(row)
+				})
+
 				if(!err){
 					resolve(subjects)
 				}else{
 					reject(err)
 				}
 
+			})
+
+		})
+
+		return promise
+
+	}
+
+	static save(param){
+
+		let promise = new Promise(function(resolve, reject){
+
+			let query = `INSERT INTO Subjects (teacher_id, subject_name, subject_code) VALUES (
+				${param.teacherName},
+				'${param.sn}',
+				'${param.sc}'
+			)`
+
+			db.run(query, function(err, rows){
+				if(!err){
+					resolve()
+				}else{
+					reject(err)
+				}
+			})
+
+		})
+
+		return promise
+
+	}
+
+	static update(param, id){
+
+		let promise = new Promise(function(resolve, reject){
+
+			let query = `UPDATE Subjects SET teacher_id = ${param.teacherName}, subject_name = '${param.sn}', subject_code = '${param.sc}' WHERE id = ${id}`
+
+		})
+
+	}
+
+	static saveSubject(param){
+
+		let promise = new Promise(function(resolve, reject){
+
+			let query = `INSERT INTO Subjects (teacher_id, subject_name, subject_code) VALUES (
+				${param.teacherName},
+				'${param.sn}',
+				'${param.sc}'
+			)`
+
+			db.run(query, function(err, rows){
+				if(!err){
+					resolve()
+				}else{
+					reject(err)
+				}
 			})
 
 		})
--- routes/subjects.js
+++ routes/subjects.js
@@ -1,11 +1,71 @@
 const express = require('express')
 const router = express.Router()
 const Subject = require('../models/subjects')
+const Teacher = require('../models/teachers')
 
 router.get('/', function(req, res){
 	Subject.findAll()
 	.then(function(subjects){
-		res.render('')
+		Teacher.findAll()
+		.then(function(teacher){
+			res.render('subjects', {
+				dataSub: subjects,
+				dataTea: teacher,
+				title: 'Subject Page'
+			})
+		})
+	})
+})
+
+router.post('/', function(req, res){
+	Subject.save(req.body)
+	.then(function(){
+		res.redirect('/subjects')
+	})
+})
+
+router.get('/edit/:id', function(req, res){
+	Subject.findById(req.params.id)
+	.then(function(subject){
+		Teacher.findAll()
+		.then(function(teachers){
+			res.render('subjectsEdit', {
+				dataSub: subject[0],
+				dataTea: teachers,
+				title: 'Edit Subject Page'
+			})
+		})
+	})
+})
+
+router.post('/edit/:id', function(req, res){
+	
+})
+
+// Teacher yang ada di Subject
+
+router.get('/teacher/:id', function(req, res){
+	Subject.findById(req.params.id)
+	.then(function(subjectID){
+		Subject.findAll()
+		.then(function(subjects){
+			Teacher.findAll()
+			.then(function(teachers){
+				res.render('subjectTeachers', {
+					dataSubId: subjectID,
+					dataSub: subjects,
+					dataTea: teachers,
+					title: 'Subject Teacher Page'
+				})
+			})
+		})
+	})
+})
+
+router.post('/teacher/:id', function(req, res){
+	Subject.saveSubject(req.body)
+	.then(function(){
+		res.redirect('/subjects')
 	})
 })
 
