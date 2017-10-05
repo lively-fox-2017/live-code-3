a3188f1290f448b64b2845323d1caae762a34747
wisnudj <jayalahwisnu@gmail.com>
2017-10-05 11:30:17 +0700

    checkpoint 2

--- app.js
+++ app.js
@@ -14,13 +14,45 @@ app.get('/', function(req, res) {
 
 app.get('/subject/list', (req, res) => {
   db.all(`SELECT * FROM Subject`, (err, rows) => {
-    res.render('subject', {dataSubject: "saya ngantuk"})
+    res.render('subject', {dataSubject: rows})
   })
 })
 
 app.get('/subject/add', (req, res) => {
-  db.all(`INSERT INTO Subject(subject_name, subject_code) Values ("${req.body.subject_name}", "${req.body.subject_code}")`)
+  res.render('subjectAdd')
 })
+
+app.post('/subject/subject/add', (req, res) => {
+  db.all(`INSERT INTO Subject(subject_name, subject_code) Values ("${req.body.subject_name}", "${req.body.subject_code}")`, (err) => {
+    res.redirect('/subject/list')
+  })
+})
+
+app.get('/subject/subject/delete/:id', (req, res) => {
+  db.all(`DELETE FROM Subject Where id = "${req.params.id}"`, (err, rows) => {
+    res.redirect('/subject/list')
+  })
+})
+
+app.get('/subject/subject/edit/:id', (req, res) => {
+  db.all(`SELECT * FROM Subject WHERE id = "${req.params.id}"`, (err, rows) => {
+    res.render('subjectEdit', {dataSubject: rows})
+  })
+})
+
+app.post('/subject/subject/edit/:id', (req, res) => {
+  db.all(`UPDATE Subject SET subject_name = '${req.body.subject_name}', subject_code = '${req.body.subject_code}' WHERE id = '${req.params.id}'`, (err, rows) => {
+    res.redirect('/subject/list')
+  })
+})
+
+app.get('/teacher/list', (req, res) => {
+  db.all(`SELECT * FROM Teacher`, (err, rows) => {
+    res.render('teacher', {dataTeacher: rows})
+  })
+})
+
+
 
 app.get('/add')
 
